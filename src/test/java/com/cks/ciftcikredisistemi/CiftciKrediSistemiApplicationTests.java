bb6507879346759b51d3ed95bea2e413eade5170
Erhan Taşkın <erhantaskin184@gmail.com>
2023-07-11 11:25:39 +0300

    first commit

---
+++ src/test/java/com/cks/ciftcikredisistemi/CiftciKrediSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cks.ciftcikredisistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiftciKrediSistemiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
