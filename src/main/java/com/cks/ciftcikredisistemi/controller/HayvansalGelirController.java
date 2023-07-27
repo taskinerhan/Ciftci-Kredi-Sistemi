6d3bf28f0d3701553a7a4f670683b649e517a68a
Erhan Taşkın <erhantaskin184@gmail.com>
2023-07-27 17:37:45 +0300

    HayvansalGelir endpoint'i eklendi

---
+++ src/main/java/com/cks/ciftcikredisistemi/controller/HayvansalGelirController.java
@@ -0,0 +1,56 @@
+package com.cks.ciftcikredisistemi.controller;
+
+import com.cks.ciftcikredisistemi.dto.HayvansalGelirDto;
+import com.cks.ciftcikredisistemi.entity.hayvan.HayvanIrk;
+import com.cks.ciftcikredisistemi.entity.hayvan.HayvansalGelir;
+import com.cks.ciftcikredisistemi.repository.HayvansalGelirRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("hayvansal-gelir-yonetimi")
+@RequiredArgsConstructor
+public class HayvansalGelirController {
+    private HayvansalGelirRepository hayvansalGelirRepository;
+    @PostMapping("")
+    public ResponseEntity<?> hayvanIrkEkle(@RequestBody HayvansalGelirDto hayvansalGelirDto) {
+        if (hayvansalGelirDto == null) {
+            return ResponseEntity
+                    .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                    .body("Hayvan ırk bilgileri geçersiz veya eksik.");
+        }
+        HayvansalGelir hayvansalGelir = new HayvansalGelir();
+        hayvansalGelir.setAd(hayvansalGelirDto.getAd());
+        hayvansalGelir.setBirim(hayvansalGelirDto.getBirim());
+        hayvansalGelir.setBirimFiyat(hayvansalGelirDto.getBirimFiyat());
+        hayvansalGelirRepository.save(hayvansalGelir);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Başarılı bir şekilde oluşturuldu");
+    }
+    @GetMapping("gelirler ")
+    public ResponseEntity<?> hayvanIrkListGetir() {
+        List<HayvansalGelir> hayvanIrkList = hayvansalGelirRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(hayvanIrkList);
+    }
+    @GetMapping("gelirler/{id}")
+    public ResponseEntity<?> hayvanIrkIdGetir(@PathVariable(name = "id") Long id) {
+        HayvansalGelir hayvansalGelir=hayvansalGelirRepository.findHayvansalGelirById(id);
+        if(hayvansalGelir==null){
+            return ResponseEntity
+                    .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                    .body("Hayvan ırk bilgileri geçersiz veya eksik.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(hayvansalGelir);
+    }
+
+
+
+
+
+
+}
