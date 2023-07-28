d0b8e7c76978b967f8d901c68d0108bc9b21db67
Erhan Taşkın <erhantaskin184@gmail.com>
2023-07-28 09:43:05 +0300

    HayvansalGiderController düzeltildi.

--- src/main/java/com/cks/ciftcikredisistemi/controller/HayvansalGiderController.java
+++ src/main/java/com/cks/ciftcikredisistemi/controller/HayvansalGiderController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HayvansalGiderController {
     private HayvansalGiderRepository hayvansalGiderRepository;
+
     @PostMapping("")
     public ResponseEntity<?> hayvanGiderEkle(@RequestBody HayvansalGiderDto hayvansalGiderDto) {
         if (hayvansalGiderDto == null) {
@@ -30,26 +31,22 @@ public class HayvansalGiderController {
 
         return ResponseEntity.status(HttpStatus.OK).body("Başarılı bir şekilde oluşturuldu");
     }
+
     @GetMapping("giderler ")
     public ResponseEntity<?> hayvanGiderListGetir() {
         List<HayvansalGider> hayvansalGiderList = hayvansalGiderRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(hayvansalGiderList);
     }
+
     @GetMapping("giderler/{id}")
     public ResponseEntity<?> hayvanGiderIdGetir(@PathVariable(name = "id") Long id) {
-        HayvansalGider hayvansalGider=hayvansalGiderRepository.findHayvansalGiderById(id);
-        if(hayvansalGider==null){
+        HayvansalGider hayvansalGider = hayvansalGiderRepository.findHayvansalGiderById(id);
+        if (hayvansalGider == null) {
             return ResponseEntity
                     .status(HttpStatus.UNPROCESSABLE_ENTITY)
                     .body("Girilen bilgiler geçersiz veya eksik.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(hayvansalGider);
     }
-
-
-
-
-
-
 }
 
