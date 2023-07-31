7540d119154b5df0b485b9808a9aa6c96ab4d3fb
Erhan Taşkın <erhantaskin184@gmail.com>
2023-07-31 10:22:04 +0300

    Gerekli düzeltmeler yapıldı.

--- src/main/java/com/cks/ciftcikredisistemi/controller/GayrimenkulVarlikController.java
+++ src/main/java/com/cks/ciftcikredisistemi/controller/GayrimenkulVarlikController.java
@@ -13,18 +13,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-
+@RequestMapping("gayrimenkul-varlik-yonetimi")
 public class GayrimenkulVarlikController {
     private final CiftciVarlikRepository ciftciVarlikRepository;
     private final GayrimenkulVarlikRepository gayrimenkulVarlikRepository;
 
-    @PostMapping("gayrimenkul-varlik-yonetimi")
+    @PostMapping("")
     public ResponseEntity<?> gayrimenkulVarlikEkle(@RequestBody GayimenkulVarlikDto gayimenkulVarlikDto) {
         if (gayimenkulVarlikDto == null) {
             return ResponseEntity
--- src/main/java/com/cks/ciftcikredisistemi/dto/GayimenkulVarlikDto.java
+++ src/main/java/com/cks/ciftcikredisistemi/dto/GayimenkulVarlikDto.java
@@ -1,8 +1,6 @@
 package com.cks.ciftcikredisistemi.dto;
 
 import com.cks.ciftcikredisistemi.enums.GayrimenkulTipi;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,11 +14,13 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class GayimenkulVarlikDto {
     private Long ciftciVarlikId;
+
     private String ada;
+
     private String parsel;
+
     private double yuzOlcumu;
 
-    @Enumerated(EnumType.STRING)
     private GayrimenkulTipi gayrimenkulTipi;
 
     private BigDecimal gayrimenkulDegeri;
