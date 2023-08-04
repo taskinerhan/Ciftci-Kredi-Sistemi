9b73f99f3ed24412a90cbe7ca5970680537e3fec
Erhan Taşkın <erhantaskin184@gmail.com>
2023-08-04 17:26:38 +0300

    Gerekli eklemeler yapıldı.

--- src/main/java/com/cks/ciftcikredisistemi/repository/HayvanIrkRepository.java
+++ src/main/java/com/cks/ciftcikredisistemi/repository/HayvanIrkRepository.java
@@ -3,7 +3,9 @@ package com.cks.ciftcikredisistemi.repository;
 import com.cks.ciftcikredisistemi.entity.hayvan.HayvanIrk;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
-public interface HayvanIrkRepository extends JpaRepository<HayvanIrk,Long> {
+public interface HayvanIrkRepository extends JpaRepository<HayvanIrk, Long> {
     HayvanIrk findHayvanIrkById(Long id);
+
 }
